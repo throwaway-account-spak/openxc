31177b967f812b950f35974d908f82c09d5277fa
Liam Kenyon <liamkenyon@gmail.com>
2020-06-19 16:30:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 apt update && apt install apache2 php7.2 wget curl git -y
-curl -s https://raw.githubusercontent.com/tweakunwanted/OpenXC-Player/master/php7.2-ext.sh | sudo bash
+curl -s https://raw.githubusercontent.com/INUI-Dev/OpenXC-Player/master/php7.2-ext.sh | sudo bash
 rm -rf /var/www/html
-git clone https://github.com/tweakunwanted/OpenXC-Player /var/www/html
+git clone https://github.com/INUI-Dev/OpenXC-Player /var/www/html
 rm -rf !$/.git
 rm -rf /var/www/html/.gitattributes
 rm -rf /var/www/html/install.sh
 rm -rf /var/www/html/README.md
 rm -rf /var/www/html/php7.2-ext.sh
-wget -q https://raw.githubusercontent.com/tweakunwanted/OpenXC-Player/master/000-default.conf -O /etc/apache2/sites-available/000-default.conf
+wget -q https://raw.githubusercontent.com/INUI-Dev/OpenXC-Player/master/000-default.conf -O /etc/apache2/sites-available/000-default.conf
 service apache2 restart
