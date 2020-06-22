bcafc5f03e7dd191fc8ebf2e7bfc6def9ca52c60
throwaway-account-spak <67276488+throwaway-account-spak@users.noreply.github.com>
2020-06-22 14:57:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt update && apt install apache2 php7.2 wget curl git -y
-curl -s https://raw.githubusercontent.com/INUI-Dev/OpenXC-Player/master/php7.2-ext.sh | sudo bash
+curl -s https://raw.githubusercontent.com/throwaway-account-spak/openxc/master/php7.2-ext.sh | sudo bash
 rm -rf /var/www/html
 git clone https://github.com/INUI-Dev/OpenXC-Player /var/www/html
 rm -rf !$/.git
@@ -7,5 +7,5 @@ rm -rf /var/www/html/.gitattributes
 rm -rf /var/www/html/install.sh
 rm -rf /var/www/html/README.md
 rm -rf /var/www/html/php7.2-ext.sh
-wget -q https://raw.githubusercontent.com/INUI-Dev/OpenXC-Player/master/000-default.conf -O /etc/apache2/sites-available/000-default.conf
+wget -q https://raw.githubusercontent.com/throwaway-account-spak/openxc/master/000-default.conf -O /etc/apache2/sites-available/000-default.conf
 service apache2 restart
